32f1e863f4c2d0e988a29ed8efc1d26262db7cf5
Marie Foussette <marie.foussette@gmail.com>
2019-05-02 21:24:47 -0400

    feat(auth): add auth-footer component

---
+++ src/app/auth/shared/components/auth-footer/auth-footer.component.spec.ts
@@ -0,0 +1,27 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {AuthFooterComponent} from './auth-footer.component';
+import {RouterTestingModule} from '@angular/router/testing';
+
+describe('AuthFooterComponent', () => {
+  let component: AuthFooterComponent;
+  let fixture: ComponentFixture<AuthFooterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AuthFooterComponent],
+      imports: [RouterTestingModule]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AuthFooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/auth/shared/shared.module.ts
+++ src/app/auth/shared/shared.module.ts
@@ -6,10 +6,11 @@ import {RouterModule} from '@angular/router';
 import {AuthHeaderComponent} from './components/auth-header/auth-header.component';
 import {AppSharedModule} from '../../shared/shared.module';
 import {AuthFormComponent} from './components/auth-form/auth-form.component';
+import {AuthFooterComponent} from './components/auth-footer/auth-footer.component';
 
 @NgModule({
-  declarations: [AuthHeaderComponent, AuthFormComponent],
-  exports: [AuthHeaderComponent, AuthFormComponent],
+  declarations: [AuthHeaderComponent, AuthFormComponent, AuthFooterComponent],
+  exports: [AuthHeaderComponent, AuthFormComponent, AuthFooterComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
