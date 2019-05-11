b537a91517d79a9c3df2b418abff0081c5650197
Marie Foussette <marie.foussette@gmail.com>
2019-05-10 21:08:28 -0400

    fix(app): various fixes

--- src/app/app/containers/app/app.component.ts
+++ src/app/app/containers/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 // app
 import { UserService } from '../../../auth/shared/services/user.service';
 import { User } from '../../../auth/shared/models/user.model';
 import { AppState } from '../../../store/app.reducer';
 import { LogInSuccess, LogOut } from '../../../store/user/actions/user.actions';
+import { getCurrentUser } from '../../../store/user/selectors/user.selectors';
+import { LifecycleComponent } from '../../../shared/components/lifecycle/lifecycle.component';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +16,19 @@ import { LogInSuccess, LogOut } from '../../../store/user/actions/user.actions';
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent implements OnInit {
-  public user: User;
+export class AppComponent extends LifecycleComponent implements OnInit {
+  public user$: Observable<User>;
+  private user: User;
 
   constructor(private userService: UserService,
               private store: Store<AppState>) {
+    super();
   }
 
   ngOnInit(): void {
+    this.user$ = this.store.select(getCurrentUser)
+      .pipe(takeUntil(this.componentDestroyed$));
+
     this.user = this.userService.persistedUser;
 
     if (this.user) {
