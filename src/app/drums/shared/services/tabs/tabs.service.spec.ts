23a564dc9d3b9a25c3d04802267af805fd7cbb52
Marie Foussette <marie.foussette@gmail.com>
2019-05-18 19:30:48 -0400

    test(tab-service): add UT

--- src/app/drums/shared/services/tabs/tabs.service.spec.ts
+++ src/app/drums/shared/services/tabs/tabs.service.spec.ts
@@ -1,12 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-
+import { SnapshotAction } from '@angular/fire/database';
+// app
 import { TabsService } from './tabs.service';
+import { Tab } from '../../models/tab.model';
 
 describe('TabsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: TabsService;
+
+  beforeEach(() => {
+    const bed = TestBed.configureTestingModule({
+      providers: [TabsService]
+    });
+
+    service = bed.get(TabsService);
+  });
 
   it('should be created', () => {
-    const service: TabsService = TestBed.get(TabsService);
     expect(service).toBeTruthy();
+  });
+
+  describe('mapTabListFromSnapshotAction', () => {
+    it('should return a list of tabs from snapshot actions', () => {
+      expect(service.mapTabListFromSnapshotAction([
+        {
+          payload: {
+            key: 'a',
+            val(): Tab {
+              return {
+                $key: 's'
+              } as Tab;
+            }
+          }
+        } as SnapshotAction<Tab>
+      ]).length).toBe(1);
+    });
   });
 });
