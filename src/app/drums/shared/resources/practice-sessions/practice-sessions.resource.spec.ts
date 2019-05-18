cf56ccc8ab3544cbbde966b39a94ac32ade12a85
Marie Foussette <marie.foussette@gmail.com>
2019-05-17 21:46:42 -0400

    test(practice-session-resource): add UT

--- src/app/drums/shared/resources/practice-sessions/practice-sessions.resource.spec.ts
+++ src/app/drums/shared/resources/practice-sessions/practice-sessions.resource.spec.ts
@@ -1,14 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { of } from 'rxjs';
 // app
 import { PracticeSessionsResource } from './practice-sessions.resource';
 import { PracticeSessionsService } from '../../services/practice-sessions/practice-sessions.service';
+import { PracticeSession } from '../../models/practice-session.model';
+import { Exercise } from '../../models/exercise.model';
 
 class AfDbMock {
   list() {
     return {
-      snapshotChanges: () => {
-      }
+      snapshotChanges: () => of({}),
+      remove: () => Promise.resolve(),
+      push: () => Promise.resolve()
     };
   }
 }
@@ -34,5 +38,62 @@ describe('PracticeSessionsResource', () => {
 
   it('should be created', () => {
     expect(resource).toBeTruthy();
+  });
+
+  describe('getSessionList$', () => {
+    it('should call list', () => {
+      spyOn(db, 'list').and.callThrough();
+      spyOn(service, 'mapSessionListFromSnapshotAction').and.returnValue(true);
+
+      resource.getSessionList$('uid');
+      expect(db.list).toHaveBeenCalledTimes(1);
+
+      resource.getSessionList$('uid').subscribe(() => {
+        expect(service.mapSessionListFromSnapshotAction).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+
+  describe('removeSession', () => {
+    it('should call list and remove', () => {
+      spyOn(db, 'list').and.callThrough();
+
+      resource.removeSession('uid', 'key');
+
+      expect(db.list).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('createSession', () => {
+    it('should call list and create', () => {
+      spyOn(db, 'list').and.callThrough();
+      spyOn((resource as any), 'removeExercise').and.returnValue({});
+
+      resource.createSession('uid', {} as PracticeSession);
+
+      expect(db.list).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('removeExercise', () => {
+    it('should remove ', () => {
+      const session = {
+        exercises: [
+          { hand: 'R', tabRef: 'qwe', repeat: 1, tab: { name: 'name', type: 'type', drumkit: true, timeSignature: '4/4', notes: [] } },
+          { hand: 'R', tabRef: 'qwe', repeat: 1, tab: { name: 'name', type: 'type', drumkit: true, timeSignature: '4/4', notes: [] } }
+        ]
+      };
+
+      const newSession = (resource as any).removeExercise(session);
+      const tabs = [];
+
+      newSession.exercises.forEach((exercise: Exercise) => {
+        if (exercise.tab) {
+          tabs.push(exercise.tab);
+        }
+      });
+
+      expect(tabs.length).toBe(0);
+    });
   });
 });
