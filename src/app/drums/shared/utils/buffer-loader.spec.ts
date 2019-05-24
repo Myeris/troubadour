4892100a115e2d62e8c7ee4e86bef6ab0dbe3d1d
Marie Foussette <marie.foussette@gmail.com>
2019-05-23 20:29:46 -0400

    test(buffer-loader): add UT

--- src/app/drums/shared/utils/buffer-loader.spec.ts
+++ src/app/drums/shared/utils/buffer-loader.spec.ts
@@ -1,9 +1,25 @@
+import { BufferLoader } from './buffer-loader';
+
 describe('BufferLoader', () => {
+  const buffer: BufferLoader = new BufferLoader(
+    new AudioContext(),
+    [
+      '1',
+      '2',
+      '3'
+    ],
+    () => true
+  );
+
   describe('loadBuffer', () => {
-      // TODO
+    // TODO
   });
 
   describe('load', () => {
-    // TODO
+    it('should should call loadBuffer', () => {
+      spyOn(buffer, 'loadBuffer').and.callFake(() => true);
+      buffer.load();
+      expect(buffer.loadBuffer).toHaveBeenCalledTimes(3);
+    });
   });
 });
