1190e7df32b2168d850b64eb4b0e965d7a769c92
Marie Foussette <marie.foussette@gmail.com>
2019-05-24 09:19:51 -0400

    test(metronome-options): add UT

--- src/app/drums/metronome/components/metronome-options/metronome-options.component.spec.ts
+++ src/app/drums/metronome/components/metronome-options/metronome-options.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 // app
 import { MetronomeOptionsComponent } from './metronome-options.component';
@@ -59,48 +59,101 @@ describe('MetronomeOptionsComponent', () => {
     expect(el.query(By.css('.options'))).toBeNull();
   });
 
-  it('should be able to set an accent', () => {
-    component.setAccent(1);
-    expect(component.accents.length).toBe(2);
-    component.setAccent(1);
-    expect(component.accents.length).toBe(1);
+  describe('accents', () => {
+    it('should return the accents', () => {
+      component.form.setControl('accents', new FormArray([
+        new FormControl(0),
+        new FormControl(1)
+      ]));
+      expect(component.accents.value.length).toBe(2);
+
+      component.form.setControl('accents', new FormArray([
+        new FormControl(0),
+        new FormControl(1),
+        new FormControl(2)
+      ]));
+      expect(component.accents.value.length).toBe(3);
+    });
   });
 
-  it('should tell if note is accent', () => {
-    expect(component.isAccent(0)).toBeTruthy();
-    expect(component.isAccent(1)).toBeFalsy();
-    expect(component.isAccent(2)).toBeFalsy();
-    expect(component.isAccent(3)).toBeFalsy();
+  describe('setAccent', () => {
+    it('should be able to set an accent', () => {
+      component.setAccent(1);
+      expect(component.accents.length).toBe(2);
+      component.setAccent(1);
+      expect(component.accents.length).toBe(1);
+    });
   });
 
-  it('should emit an event to start metronome', () => {
-    const spy = spyOn(component.played, 'emit').and.callThrough();
-    component.play();
-    expect(spy).toHaveBeenCalledWith(component.form.value);
+  describe('isAccent', () => {
+    it('should tell if note is accent', () => {
+      expect(component.isAccent(0)).toBeTruthy();
+      expect(component.isAccent(1)).toBeFalsy();
+      expect(component.isAccent(2)).toBeFalsy();
+      expect(component.isAccent(3)).toBeFalsy();
+    });
   });
 
-  it('should emit and event to stop metronome', () => {
-    const spy = spyOn(component.stopped, 'emit').and.callThrough();
-    component.stop();
-    expect(spy).toHaveBeenCalled();
+  describe('play', () => {
+    it('should emit an event to start metronome', () => {
+      spyOn(component.played, 'emit').and.callThrough();
+      component.play();
+      expect(component.played.emit).toHaveBeenCalledTimes(1);
+      expect(component.played.emit).toHaveBeenCalledWith(component.form.value);
+    });
   });
 
-  it('should increase the bpm value', () => {
-    component.form.get('bpm').setValue(40);
-    component.changeBpm(false);
-    expect(component.form.get('bpm').value).toBe(40);
+  describe('stop', () => {
+    it('should emit and event to stop metronome', () => {
+      spyOn(component.stopped, 'emit').and.callThrough();
 
-    component.form.get('bpm').setValue(250);
-    component.changeBpm(true);
-    expect(component.form.get('bpm').value).toBe(250);
+      component.stop();
+      expect(component.stopped.emit).toHaveBeenCalledTimes(1);
+      expect(component.playing).toBeFalsy();
+    });
+  });
 
-    component.form.get('bpm').setValue(60);
-    component.changeBpm(false);
-    expect(component.form.get('bpm').value).toBe(59);
+  describe('changeBpm', () => {
+    it('should increase the bpm value', () => {
+      component.form.get('bpm').setValue(40);
+      component.changeBpm(false);
+      expect(component.form.get('bpm').value).toBe(40);
 
-    component.form.get('bpm').setValue(60);
-    component.changeBpm(true);
-    expect(component.form.get('bpm').value).toBe(61);
+      component.form.get('bpm').setValue(250);
+      component.changeBpm(true);
+      expect(component.form.get('bpm').value).toBe(250);
+
+      component.form.get('bpm').setValue(60);
+      component.changeBpm(false);
+      expect(component.form.get('bpm').value).toBe(59);
+
+      component.form.get('bpm').setValue(60);
+      component.changeBpm(true);
+      expect(component.form.get('bpm').value).toBe(61);
+    });
+  });
+
+  describe('restart', () => {
+    it('should to nothing', () => {
+      spyOn(component, 'stop').and.callFake(() => true);
+      spyOn(component, 'play').and.callFake(() => true);
+
+      component.playing = false;
+      component.restart();
+      expect(component.stop).not.toHaveBeenCalled();
+      expect(component.play).not.toHaveBeenCalled();
+    });
+
+    it('should stop and replay', fakeAsync(() => {
+      spyOn(component, 'stop').and.callFake(() => true);
+      spyOn(component, 'play').and.callFake(() => true);
+
+      component.playing = true;
+      component.restart();
+      expect(component.stop).toHaveBeenCalled();
+      tick(500);
+      expect(component.play).toHaveBeenCalled();
+    }));
   });
 });
 
