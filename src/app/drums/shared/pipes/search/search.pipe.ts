f6b8c8851bc227f3e60f8ad5590628020c81988e
Marie Foussette <marie.foussette@gmail.com>
2019-05-26 18:09:13 -0400

    fix(search): fix search code

--- src/app/drums/shared/pipes/search/search.pipe.ts
+++ src/app/drums/shared/pipes/search/search.pipe.ts
@@ -9,22 +9,9 @@ export class SearchPipe implements PipeTransform {
       return value;
     }
 
-    const keys = args[0].split('.');
+    const key: string = args[0];
+    const search = args[1];
 
-    // TODO avoid for in loop
-    return value.filter((v: any) => {
-      let obj = v;
-
-      // tslint:disable-next-line:prefer-for-of
-      for (let i = 0; i < keys.length; i++) {
-        obj = obj[keys[i]];
-      }
-
-      if (typeof obj !== 'string') {
-        return v;
-      }
-
-      return obj.toLowerCase().includes(args[1].toLowerCase());
-    });
+    return value.filter((v => v[key] === search));
   }
 }
