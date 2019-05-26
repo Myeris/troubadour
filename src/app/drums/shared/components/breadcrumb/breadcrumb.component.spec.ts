678af322be97021ddb8d30e857e1074a7b72ac6e
Marie Foussette <marie.foussette@gmail.com>
2019-05-26 17:33:37 -0400

    test(breadcrumn): add UT

--- src/app/drums/shared/components/breadcrumb/breadcrumb.component.spec.ts
+++ src/app/drums/shared/components/breadcrumb/breadcrumb.component.spec.ts
@@ -2,13 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 // app
 import { BreadcrumbComponent } from './breadcrumb.component';
+import { asPureExpressionData } from '@angular/core/src/view';
 
 describe('BreadcrumbComponent', () => {
   let component: BreadcrumbComponent;
   let fixture: ComponentFixture<BreadcrumbComponent>;
   let el: DebugElement;
+  let router: Router;
 
   beforeEach(() => {
     const bed = TestBed.configureTestingModule({
@@ -23,6 +26,7 @@ describe('BreadcrumbComponent', () => {
     fixture = bed.createComponent(BreadcrumbComponent);
     component = fixture.componentInstance;
     el = fixture.debugElement;
+    router = bed.get(Router);
   });
 
   it('should be defined', () => {
@@ -37,5 +41,18 @@ describe('BreadcrumbComponent', () => {
     };
     fixture.detectChanges();
     expect(el.query(By.css('a')).nativeElement.textContent).toContain('◀ Route 1');
+  });
+
+  describe('navigate', () => {
+    it('should navigate', () => {
+      spyOn(router, 'navigate').and.callFake(() => true);
+      component.breadcrumb = {
+        label: 'Route 1',
+        route: '/route1',
+        params: {}
+      };
+      component.navigate();
+      expect(router.navigate).toHaveBeenCalledTimes(1);
+    });
   });
 });
